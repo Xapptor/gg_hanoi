233d2fd26557cf961ddda746ff008a73eebb7b22
Javier Garcia <javierjesusgarciacontreras@gmail.com>
2025-11-28 05:25:24 -0600

    Refactor customSpacer for concise SizedBox syntax
    Simplified the customSpacer widget by using a single-line SizedBox constructor for improved readability.

--- lib/values.dart
+++ lib/values.dart
@@ -5,9 +5,7 @@ import 'package:gg_hanoi/extensions.dart';
 Widget customSpacer({
   double multiplier = 1,
 }) =>
-    SizedBox(
-      height: 24 * multiplier,
-    );
+    SizedBox(height: 24 * multiplier);
 
 Color getRandomColor() => ColorExtension.fromHex('#${Random().nextInt(0xFFFFFF).toRadixString(16).padLeft(6, '0')}');
 
